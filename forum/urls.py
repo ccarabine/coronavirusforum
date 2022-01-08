3b560dcbfaacc1acc78e0efa985d0e890dc6018e
ccarabine <ccarabine12@gmail.com>
2022-01-08 16:36:44 +0000

    Create postlist and pagination WIP

--- forum/urls.py
+++ forum/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home')
+    path('', views.PostListHome.as_view(), name='home'),
+    path('postlist', views.PostList.as_view(), name='postlist'),
 ]
--- forum/views.py
+++ forum/views.py
@@ -1,13 +1,18 @@
-from django.shortcuts import render
-from django.views import generic
-from django.views.generic import ListView
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 
+class PostListHome(ListView):
+    model = Post
+    queryset = Post.objects.order_by('-created')[:5]
+    template_name = 'index.html'
+    context_object_name = 'latest_posts'
+
 class PostList(ListView):
     model = Post
-    latest_posts_queryset = Post.objects.order_by('-created')[:5]
     queryset = Post.objects.order_by('-created')
-    template_name = 'index.html'
+    template_name = 'post_list.html'
     context_object_name = 'post_list'
     paginate_by = 5
