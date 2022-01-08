a1824fbc7772e3a21cf8961fbc939d020ac9a52a
ccarabine <ccarabine12@gmail.com>
2022-01-08 14:44:45 +0000

    Update index.html with latest 5 posts

--- forum/views.py
+++ forum/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.views import generic
+from django.views.generic import ListView
 from .models import Post
 
 
-class PostList(generic.ListView):
+class PostList(ListView):
     model = Post
+    latest_posts_queryset = Post.objects.order_by('-created')[:5]
     queryset = Post.objects.order_by('-created')
     template_name = 'index.html'
+    context_object_name = 'post_list'
     paginate_by = 5
